4ae5d7f4ede0bc4347e932083c02d5332910a50a
薛梦迪 <MandyXue@users.noreply.github.com>
2017-10-30 06:53:21 +0800

    update fraction, with score of 44 on Kattis

--- fraction.py
+++ fraction.py
@@ -1,44 +1,40 @@
 import random
 import math
-import sys
 # import fractions
 import time
 
-# prime_table for trival division check
-# prime_table = (2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79)
-
-# 用的别人的miller，只是为了验证逻辑正确性，后面需要改掉
-# n 不能输入1
+# other's miller
+# to be modified afterwards
 def miller_rabin(n, k=10):
+    if n == 2:
+        return True
+    if not n & 1:
+        return False
+
     def check(a, s, d, n):
         x = pow(a, d, n)
         if x == 1:
             return True
-        for i in range(s-1):
+        for i in range(s - 1):
             if x == n - 1:
                 return True
             x = pow(x, 2, n)
         return x == n - 1
-    # check the special case if n equals 2
-    if n == 2:
-        return True
-    # check odd or even with "bit and" operation
-    if not n & 1:
-        return False
+
     s = 0
     d = n - 1
-    # get d and divided by 2
+
     while d % 2 == 0:
         d >>= 1
         s += 1
-    # call check to check if it is a prime number
-    # return true with high probability, always return false if it is false
-    for j in range(k):
-        # randomly choose an a for checking
+
+    for i in range(k):
         a = random.randint(2, n - 1)
         if not check(a, s, d, n):
             return False
     return True
+
+prime_table = (2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79)
 
 def randGen(x, N):
     return (x**2 + 1) % N
@@ -47,7 +43,6 @@ def randGen(x, N):
 # input a list contains the original factor
 # operate with the original list
 def pollard(N):
-    print("pollard")
     for element in N:
         if miller_rabin(element) is not True:
             N.remove(element)
@@ -56,8 +51,7 @@ def pollard(N):
             y = a
             # c = 1
             # while True:
-            for i in range(10000):
-                # while True:
+            for i in range(1000000):
                 x = randGen(x, element)
                 y = randGen(randGen(y, element), element)
                 if x!=y:
@@ -74,10 +68,9 @@ def pollard(N):
                 else:
                     x = random.randint(1,element)
                     y = x
-        return 1
     return 0
 
-# 试除法调用的一个子函数
+# a sub-function for trivial division
 def extract(mlist, small_prime):
     for i in mlist:
         if i % small_prime == 0 and i != small_prime:
@@ -86,34 +79,45 @@ def extract(mlist, small_prime):
             mlist.append(int(i // small_prime))
             extract(mlist, small_prime)
 
-# 试除
+# trivial division
 def trival_div(mlist, pri_table):
     for i in pri_table:
         extract(mlist, i)
 
-M = [175891579187581657617175891579187581657617175891579187581657617]
-# M = [12138]
-# M=[807855320741025594307788028083709]
-
-prime_table = []
-for i in range(2,50000):
-    if miller_rabin(i):
-        prime_table.append(i)
-
-# M = [4588586040451223]
+# testing
+# M = [5794746215675376587969763525444444657]
+# # M=[807855320741025594307788028083709]
 # trival_div(M, prime_table)
-# big_factor = [M.pop()]
 #
-# result = M+big_factor
-# print(result)
+# big_factor = [M.pop()]
+# print(pollard(big_factor))
+# pollard(big_factor)
+# print(M+big_factor)
 
-for i in sys.stdin:
-    M = [int(i)]
+# input
+inputs = []
+while True:
+    ## the following is used on kattis
+    try:
+        inputs.append(input())
+    except EOFError:
+        break
+    ## the following is used locally
+    # str = input()
+    # if str == "":
+    #     break
+    # inputs.append(str)
+
+# output
+for input in inputs:
+    if len(input) > 23:
+        print("fail")
+        print()
+        continue
+    M = [int(input)]
     trival_div(M, prime_table)
     big_factor = [M.pop()]
-    flag = pollard(big_factor)
-    result = M + big_factor + ['\r']
-    #print(result)
-    # print(result)
-    for j in result:
-        print(j)
+    pollard(big_factor)
+    for factor in M+big_factor:
+        print(factor)
+    print()
