48b08264cefabccb92f09f8b457cecaf86ed68fd
Celiali <33164692+Celiali@users.noreply.github.com>
2017-10-30 00:47:24 +0100

    table change, length check (61 on Kattis)
    Generate a 10w prime table, and check the last element length (23) after trival division, new miller_rabin function (23 for Score 61 on Kattis, 24 length for score 67)

--- fraction.py
+++ fraction.py
@@ -3,14 +3,9 @@ import math
 # import fractions
 import time
 
-# other's miller
+
 # to be modified afterwards
 def miller_rabin(n, k=10):
-    if n == 2:
-        return True
-    if not n & 1:
-        return False
-
     def check(a, s, d, n):
         x = pow(a, d, n)
         if x == 1:
@@ -21,23 +16,33 @@ def miller_rabin(n, k=10):
             x = pow(x, 2, n)
         return x == n - 1
 
+    # check the special case if n equals 2
+    if n == 2:
+        return True
+    # check odd or even with "bit and" operation
+    if not n & 1:
+        return False
     s = 0
     d = n - 1
-
+    # get d and divided by 2
     while d % 2 == 0:
         d >>= 1
         s += 1
-
-    for i in range(k):
+    # call check to check if it is a prime number
+    # return true with high probability, always return false if it is false
+    for j in range(k):
+        # randomly choose an a for checking
         a = random.randint(2, n - 1)
         if not check(a, s, d, n):
             return False
     return True
 
-prime_table = (2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79)
+
+# prime_table = (2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79)
 
 def randGen(x, N):
-    return (x**2 + 1) % N
+    return (x ** 2 + 1) % N
+
 
 # pollard
 # input a list contains the original factor
@@ -54,21 +59,22 @@ def pollard(N):
             for i in range(1000000):
                 x = randGen(x, element)
                 y = randGen(randGen(y, element), element)
-                if x!=y:
+                if x != y:
                     # d = fractions.gcd(abs(x-y), N)
-                    d = math.gcd(int(abs(x-y)), element)
-                    if (d > 1) and (d<element):
+                    d = math.gcd(int(abs(x - y)), element)
+                    if (d > 1) and (d < element):
                         # return d
                         N.append(d)
-                        N.append(element//d)
+                        N.append(element // d)
                         pollard(N)
                         return 1
                         # fraction_set.add(d)
                         # print(d)
                 else:
-                    x = random.randint(1,element)
+                    x = random.randint(1, element)
                     y = x
     return 0
+
 
 # a sub-function for trivial division
 def extract(mlist, small_prime):
@@ -79,10 +85,12 @@ def extract(mlist, small_prime):
             mlist.append(int(i // small_prime))
             extract(mlist, small_prime)
 
+
 # trivial division
 def trival_div(mlist, pri_table):
     for i in pri_table:
         extract(mlist, i)
+
 
 # testing
 # M = [5794746215675376587969763525444444657]
@@ -94,6 +102,12 @@ def trival_div(mlist, pri_table):
 # pollard(big_factor)
 # print(M+big_factor)
 
+prime_table = []
+
+for i in range(2, 100000):
+    if miller_rabin(i):
+        prime_table.append(i)
+
 # input
 inputs = []
 while True:
@@ -102,22 +116,30 @@ while True:
         inputs.append(input())
     except EOFError:
         break
-    ## the following is used locally
-    # str = input()
-    # if str == "":
-    #     break
-    # inputs.append(str)
+        ## the following is used locally
+        # str = input()
+        # if str == "":
+        #     break
+        # inputs.append(str)
 
 # output
 for input in inputs:
-    if len(input) > 23:
+    M = [int(input)]
+    trival_div(M, prime_table)
+
+    if miller_rabin(M[-1]) is True:
+        for factor in M:
+            print(factor)
+        print()
+        continue
+
+    if len(str(M[-1])) > 23:
         print("fail")
         print()
         continue
-    M = [int(input)]
-    trival_div(M, prime_table)
+
     big_factor = [M.pop()]
     pollard(big_factor)
-    for factor in M+big_factor:
+    for factor in M + big_factor:
         print(factor)
     print()
