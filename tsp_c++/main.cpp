6330432f8738d8156ab41fabfcdcc1a5b7e76fa8
MandyXue <13564039693@163.com>
2017-12-01 21:13:22 +0100

    modify c++ out

---
+++ tsp_c++/main.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <vector>
+#include "node.h"
+#include "state.h"
+
+using namespace std;
+
+State simulatedAnnealing(State s, double Tmin, double T, double r , int stop_iteration );
+
+int main() {
+    int num;
+    State s;
+    cin>> num;
+    for(int i = 0; i < num;i++) {
+        double x, y;
+        cin >> x >> y;
+        Node node(x, y, i);
+        s.n.push_back(node);
+    }
+    if(num ==1 )
+        cout << s.n[num-1].number;
+    else{
+        random_shuffle(s.n.begin(), s.n.end());
+
+//      测试输出
+//      s.print_node();
+
+//        default_random_engine defaultEngine;
+//        shuffle(s.n.begin(), s.n.end(),defaultEngine);
+        int itera = 1;
+        if (num <500)
+            itera = 100000;
+        else
+            itera = 10000;
+        State newState = simulatedAnnealing(s,0.01,100.0,0.9999,itera);
+
+//      测试输出
+//        cout<< "evaluation"<< newState.evaluation()<<endl;
+//        cout<< "number"<<endl;
+        for(int i = 0; i< newState.n.size()-1;i++)
+            cout << newState.n[i].number<<endl;
+        cout<<newState.n[num-1].number;
+    }
+
+    return 0;
+}
+
+State simulatedAnnealing(State s, double Tmin, double T, double r , int stop_iteration ){
+
+    State originalState;
+    double originalEval = s.evaluation();
+    originalState.n.assign(s.n.begin(), s.n.end());
+    //      测试输出
+//    cout<<"originalState";
+//    originalState.print_node();
+
+    State miniState;
+    double miniEval = originalEval;
+    //      测试输出
+//    cout<<"miniState";
+//    miniState.print_node();
+
+    int iter = 0;
+
+    double secondEval;
+    State secondState;
+    for (int i = 0;i<3;i++){
+        while(T > Tmin){
+            secondState.n = originalState.getRandomNewState();
+            secondEval = secondState.evaluation();
+            //测试输出
+//            cout<<"secondState";
+//            secondState.print_node();
+
+            if (iter >= stop_iteration)
+                break;
+            iter++;
+            double dE = originalEval-secondEval;
+
+//            测试输出
+//            cout<< "secondEval"<<secondEval<<endl;
+            if(dE >= 0)
+            {
+                originalEval = secondEval;
+                originalState.n = secondState.n;
+
+                if(secondEval < miniEval) {
+                    miniState.n = secondState.n;
+                    miniEval = secondEval;
+                         // 测试输出
+                   // cout << "miniState";
+                   // miniState.print_node();
+                }
+//                测试输出
+//                cout<<"originalState";
+//                originalState.print_node();
+            }
+            else{
+//                std::default_random_engine random(time_t(NULL));
+                std::random_device rd;
+                std::default_random_engine gen =std::default_random_engine(rd());
+                std::uniform_real_distribution<double> dis2(0.0, 1.0);
+//                srand(time(NULL));
+//                float ran = rand() % (N + 1) / (float) (N + 1);//生成0-1间的随机数。
+                double ran = dis2(gen);
+                double temp =exp(dE/T);
+                if ( temp > ran){
+                    originalEval = secondEval;
+                    originalState.n = secondState.n;
+
+//                    测试输出
+//                    cout<<"originalState";
+//                    originalState.print_node();
+                }
+            }
+            T = r*T;
+        }
+        T = 40/(i+1);
+    }
+//    return originalState;
+    return miniState;
+}
---
+++ tsp_c++/state.h
@@ -0,0 +1,79 @@
+//
+// Created by lici on 17-11-28.
+//
+#ifndef TSP_STATE_H
+#define TSP_STATE_H
+
+#include <vector>
+#include <algorithm>
+#include "node.h"
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+class State
+{
+public:
+    vector<Node> n;
+ //   double evalue;
+
+//    State(){
+//        evalue = evaluation();
+//    }
+
+    unsigned long getlength(){
+       return n.size();
+    }
+
+    void addNode(Node a){
+        n.push_back(a);
+    }
+
+    vector<Node> getRandomNewState(){
+        unsigned long length = n.size();
+
+//        std::default_random_engine random(time(NULL));
+        std::random_device rd;
+        std::default_random_engine gen =std::default_random_engine(rd());
+        std::uniform_int_distribution<int> dis1(0, int(length)-1);
+//        srand((unsigned)time(NULL));
+//        unsigned long i = (rand() % (length));
+//        unsigned long j = (rand() % (length));;
+
+        int i = dis1(gen);
+        int j = dis1(gen);
+        while(j == i)
+            j = dis1(gen);
+        auto it_i = n.begin()+i;
+        Node temp = *it_i;
+        n.erase(it_i);
+        auto it_j=n.begin()+j;
+        n.insert(it_j, temp);
+        return n;
+    }
+
+    double evaluation(){
+        double distance = 0.0;
+        unsigned long length = n.size();
+        for(int i =0; i< length-1; i++)
+        {
+            double d = sqrt(pow((n[i+1].xPost-n[i].xPost),2)+pow((n[i+1].yPost-n[i].yPost),2));
+            distance += d;
+        }
+        double d = sqrt(pow((n[0].xPost-n[length-1].xPost),2)+pow((n[0].yPost-n[length-1].yPost),2));
+        distance += d;
+        return distance;
+    }
+
+    void print_node()
+    {
+        std::cout<<"print node\n";
+        for (std::vector<Node>::iterator it= n.begin(); it!= n.end(); ++it)
+            std::cout << ' ' << (*it).number<<" ";
+        std::cout << '\n';
+    }
+};
+
+
+#endif //TSP_STATE_H
